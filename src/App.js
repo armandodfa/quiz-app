b85e9f39723c5c914805da9e0982e9ccb66aac21
Arman Razdyk <armanrazdyk@MacBook-Air-Arman-331.local>
2022-09-21 11:22:12 +0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariants }) {
+  const persentage = Math.round((step / questions.length) * 100);
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${persentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li
+            onClick={() => {
+              onClickVariants(index);
+            }}
+            key={text}
+          >
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function App() {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+  const onClickVariants = (index) => {
+    setStep(step + 1);
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+  return (
+    <div className="App">
+      {step !== questions.length ? (
+        <Game
+          step={step}
+          question={question}
+          onClickVariants={onClickVariants}
+        />
+      ) : (
+        <Result correct={correct} />
+      )}
+    </div>
+  );
+}
+
+export default App;
